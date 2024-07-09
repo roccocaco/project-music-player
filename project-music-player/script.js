b644d90995b504147b7252aedbe61e2aafdc2b56
Rocco Caco <32roccocaco@gmail.com>
2024-07-08 22:29:52 -0300

    update

--- project-music-player/script.js
+++
@@ -1,6 +0,0 @@
-let currectMusic = 0;
-
-const music = document.querySelector("#audio");
-
-const seekBar = document.querySelector(".seek-bar");
-const songName = document.querySelector(".music-name");
