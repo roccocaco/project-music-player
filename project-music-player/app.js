efa292ff042271ed7e69ba34dbefe1153ec02ace
Rocco Caco <32roccocaco@gmail.com>
2024-07-08 22:54:48 -0300

    update

--- project-music-player/app.js
+++ project-music-player/app.js
@@ -7,12 +7,17 @@ const songName = document.querySelector(".music-name");
 const artistName = document.querySelector(".artist-name");
 const disk = document.querySelector(".disk");
 const currentTime = document.querySelector(".current-time");
-const musicDuration = document.querySelector(".song-duration");
+const musicDuration = document.querySelector(".music-duration");
 const playBtn = document.querySelector(".play-btn");
 const forwardBtn = document.querySelector(".forward-btn");
 const backwardBtn = document.querySelector(".backward-btn");
 
 playBtn.addEventListener("click", () => {
+  if (playBtn.className.includes("pause")) {
+    music.play();
+  } else {
+    music.pause();
+  }
   playBtn.classList.toggle("pause");
   disk.classList.toggle("play");
 });
@@ -20,20 +25,68 @@ playBtn.addEventListener("click", () => {
 // setup music
 
 const setMusic = (i) => {
-    seekBar.value = 0;
-    let song = song[i];
-    currentMusic = i;
-    music.src = song.path;
+  seekBar.value = 0;
+  let song = song[i];
+  currentMusic = i;
+  music.src = song.path;
 
-    songName.inner = song.name;
-    artistName.inner = song.name;
-    disk.style.backgroundImage = `url('${song.cover}')`;
+  songName.inner = song.name;
+  artistName.inner = song.name;
+  disk.style.backgroundImage = `url('${song.cover}')`;
 
-    currentTime.innerHTML = '00:00';
-    setTimeout(() => {
-      seekBar.max = music.duration;
-      musicDuration.innerHTML = formatTime(music.duration)
-    }, 300);
-}
+  currentTime.innerHTML = "00:00";
+  setTimeout(() => {
+    seekBar.max = music.duration;
+    musicDuration.innerHTML = formatTime(music.duration);
+  }, 300);
+};
 
 setMusic(0);
+
+const formatTime = (time) => {
+  let min = Math.floor(time / 60);
+  if (min < 10) {
+    min - `0${min}`;
+  }
+  let sec = Math.floor(time % 60);
+  if (sec < 10) {
+    sec = `0${sec}`;
+  }
+  return `${min} : ${sec}`;
+};
+
+setInterval(() => {
+  seekBar.value = music.currentTime;
+  currentTime.innerHTML = formatTime(music.currentTime);
+  if(Math.floor(music.currentTime) == Math.floor(seekBar.max))
+    forwardBtn.click();
+}, 500);
+
+seekBar.addEventListener("change", () => {
+  music.currentTime = seekBar.value;
+});
+
+const playMusic = () => {
+  music.play();
+  playBtn.classList.remove("pause");
+  disk.classList.add("play");
+};
+
+forwardBtn.addEventListener("click", () => {
+  if (currentMusic > -songs.length - 1) {
+    currentMusic = 0;
+  } else {
+    currentMusic++;
+  }
+});
+
+backwardBtn.addEventListener("click", () => {
+  if (currentMusic <= 0) {
+    currectMusic = songs.length - 1;
+  } else {
+    currentMusic--;
+  }
+  setMusic(currectMusic);
+  playBtn();
+});
+
--- project-music-player/data.js
+++ project-music-player/data.js
@@ -1,62 +1,62 @@
 let songs = [
-    {
-        name: 'you give good love',
-        path: '',
-        artist: 'Whitney Houston',
-        cover: 'img'
-    },
-    {
-        name: 'thinking about you',
-        path: '',
-        artist: 'Whitney Houston',
-        cover: 'img'
-    },
-    {
-        name: 'someone for me',
-        path: '',
-        artist: 'Whitney Houston',
-        cover: 'img'
-    },
-    {
-        name: 'saving all my love for you',
-        path: '',
-        artist: 'Whitney Houston',
-        cover: 'img'
-    },
-    {
-        name: 'nobody loves me like you do',
-        path: '',
-        artist: 'Whitney Houston',
-        cover: 'img'
-    },
-    {
-        name: 'how will i know',
-        path: '',
-        artist: 'Whitney Houston',
-        cover: 'img'
-    },
-    {
-        name: 'all at once',
-        path: '',
-        artist: 'Whitney Houston',
-        cover: 'img'
-    },
-    {
-        name: 'take good care of my heart',
-        path: '',
-        artist: 'Whitney Houston',
-        cover: 'img'
-    },
-    {
-        name: 'greatest love of all',
-        path: '',
-        artist: 'Whitney Houston',
-        cover: 'img'
-    },
-    {
-        name: 'hold me',
-        path: '',
-        artist: 'Whitney Houston',
-        cover: 'img'
-    },
-]
+  {
+    name: "you give good love",
+    path: "",
+    artist: "Whitney Houston",
+    cover: "img",
+  },
+  {
+    name: "thinking about you",
+    path: "",
+    artist: "Whitney Houston",
+    cover: "img",
+  },
+  {
+    name: "someone for me",
+    path: "",
+    artist: "Whitney Houston",
+    cover: "img",
+  },
+  {
+    name: "saving all my love for you",
+    path: "",
+    artist: "Whitney Houston",
+    cover: "img",
+  },
+  {
+    name: "nobody loves me like you do",
+    path: "",
+    artist: "Whitney Houston",
+    cover: "img",
+  },
+  {
+    name: "how will i know",
+    path: "",
+    artist: "Whitney Houston",
+    cover: "img",
+  },
+  {
+    name: "all at once",
+    path: "",
+    artist: "Whitney Houston",
+    cover: "img",
+  },
+  {
+    name: "take good care of my heart",
+    path: "",
+    artist: "Whitney Houston",
+    cover: "img",
+  },
+  {
+    name: "greatest love of all",
+    path: "",
+    artist: "Whitney Houston",
+    cover: "img",
+  },
+  {
+    name: "hold me",
+    path: "",
+    artist: "Whitney Houston",
+    cover: "img",
+  },
+];
